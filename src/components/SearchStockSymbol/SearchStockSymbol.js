af2b050e1d60c5039158162fdad2ce19f9ad106c
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-09-23 21:11:48 -0400

    feat: search and onsubmit functionality

--- src/components/SearchStockSymbol/SearchStockSymbol.js
+++ src/components/SearchStockSymbol/SearchStockSymbol.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import { useTranslation } from "react-i18next";
 
@@ -6,14 +6,34 @@ import { useTranslation } from "react-i18next";
 //pass style
 //pass the action
 
-const SearchStockSymbol = () => {
+//input
+//autocomplete
+const SearchStockSymbol = (props) => {
+  const [searchterm, setSearchterm] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    props.onSubmit(searchterm);
+  };
+
   const { t, i18n } = useTranslation();
   return (
-    <div className="search-wrapper">
-      <form>
+    <div className="search-bar-wrapper">
+      <form onSubmit={(event) => this.onFormSubmit(event)}>
         <label htmlFor="search">Search</label>
-        <input id="search" type="text" name="search"></input>
-        <Button type="submit" label={t("hello")} />
+        <input
+          id="search"
+          type="text"
+          name="search"
+          value={searchterm}
+          onChange={(e) => setSearchterm(e.target.value)}
+        ></input>
+        <Button
+          className="submit-button"
+          onClick={(event) => onFormSubmit(event)}
+          type="submit"
+          label={t("hello")}
+        />
       </form>
       <hr />
     </div>
