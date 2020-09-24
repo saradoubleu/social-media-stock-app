8abb665706cebbedbb8b0148540e0a7c8c44d67b
sara womi <17014669+saradoubleu@users.noreply.github.com>
2020-09-23 20:34:15 -0400

    feat: component framework

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import Header from "./Header/Header";
+import SearchStocks from "./SearchStockSymbol/SearchStockSymbol";
+import Suggestions from "./Suggestions/Suggestions";
 
 export default class App extends Component {
   render() {
-    return <div>Sara </div>;
+    return (
+      <div className="stock-wrapper">
+        <Header />
+        <SearchStocks />
+        <Suggestions />{" "}
+      </div>
+    );
   }
 }
---
+++ src/components/Suggestions/Suggestions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Suggestions = () => {
+  return <div> Suggestsions</div>;
+};
+
+export default Suggestions;
